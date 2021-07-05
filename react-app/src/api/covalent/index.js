942cab721e4cef4835ad731f1a87a5a68dd45d3e
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-07-06 02:54:24 +0700

    comment code

--- react-app/src/api/covalent/index.js
+++ react-app/src/api/covalent/index.js
@@ -1,20 +1,28 @@
 import axios from 'axios';
 
-export const client = axios.create({ baseURL: process.env.REACT_APP_COVALENT_API_AWS_URL });
+// initial requester object
+export const requester = axios.create({ baseURL: process.env.REACT_APP_COVALENT_API_AWS_URL });
 
+// function to request data from your API on AWS by passing 2 arguments (path, params)
 export const request = async (path, params) => {
+  // response data variable
   let response = null;
 
   try {
-    const res = await client.get('', { params: { path, ...(params || {}) } })
+    // send request to your API
+    const res = await requester.get('', { params: { path, ...(params || {}) } })
+      // set response data from error handled by exception
       .catch(error => { return { data: null, error: true, error_message: error.message, error_code: error.code }; });
 
+    // set response data
     if (res && res.data) {
       response = res.data;
     }
   } catch (error) {
+    // set response data from error handled by exception
     response = { data: null, error: true, error_message: error.message, error_code: error.code };
   }
 
+  // return response data
   return response;
 };
