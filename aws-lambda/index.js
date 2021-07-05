121d2253cf0cd11666c96f44ed822ef0b70a3414
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-07-06 02:34:25 +0700

    indent 2 spaces

--- aws-lambda/index.js
+++ aws-lambda/index.js
@@ -3,48 +3,48 @@
  * Deploy on AWS Lambda (triggered by AWS API Gateway)
  ************************************************/
 exports.handler = async (event, context, callback) => {
-	// import module for submitting request.
-	const axios = require('axios');
+  // import module for submitting request.
+  const axios = require('axios');
 
-	/************************************************
-	 * Covalent API information for requesting data
-	 * You can setup these environment variables below on the AWS Lambda function's configuration.
-	 * - API_HOST (Optional)
-	 * - API_KEY (You can also replace '{YOUR_API_KEY}' with your key if you are not set it on Lambda function)
-	 ************************************************/
-	const api_host = process.env.API_HOST || 'https://api.covalenthq.com/v1/';
-	const api_key = process.env.API_KEY || '{YOUR_API_KEY}';
+  /************************************************
+   * Covalent API information for requesting data
+   * You can setup these environment variables below on the AWS Lambda function's configuration.
+   * - API_HOST (Optional)
+   * - API_KEY (You can also replace '{YOUR_API_KEY}' with your key if you are not set it on Lambda function)
+   ************************************************/
+  const api_host = process.env.API_HOST || 'https://api.covalenthq.com/v1/';
+  const api_key = process.env.API_KEY || '{YOUR_API_KEY}';
 
   // response data variable
-	let response = null;
+  let response = null;
 
-	// check path parameter exist
-	if (event.queryStringParameters && event.queryStringParameters.path) {
-		// initial requester object
-		const requester = axios.create({ baseURL: api_host });
+  // check path parameter exist
+  if (event.queryStringParameters && event.queryStringParameters.path) {
+    // initial requester object
+    const requester = axios.create({ baseURL: api_host });
 
-		// normalize path parameter
-		const path = `${event.queryStringParameters.path}${!event.queryStringParameters.path.endsWith('/') ? '/' : ''}`;
-		// remove path parameter before setup query string parameters
-		delete event.queryStringParameters.path;
-		// setup query string parameters including API key
-		const params = { key: api_key, ...event.queryStringParameters };
+    // normalize path parameter
+    const path = `${event.queryStringParameters.path}${!event.queryStringParameters.path.endsWith('/') ? '/' : ''}`;
+    // remove path parameter before setup query string parameters
+    delete event.queryStringParameters.path;
+    // setup query string parameters including API key
+    const params = { key: api_key, ...event.queryStringParameters };
 
-		// send request to Covalent API
-		const res = await requester.get(path, { params })
-			// set response data from error handled by exception
-			.catch(error => { return { data: null, error: true, error_message: error.message, error_code: error.code }; });
+    // send request to Covalent API
+    const res = await requester.get(path, { params })
+      // set response data from error handled by exception
+      .catch(error => { return { data: null, error: true, error_message: error.message, error_code: error.code }; });
 
-		// set response data
-		if (res && res.data) {
-			response = res.data;
-		}
-	}
-	else {
-		// set response data to 'Not found'
-		response = { data: null, error: true, error_message: 'Not found', error_code: 404 };
-	}
+    // set response data
+    if (res && res.data) {
+      response = res.data;
+    }
+  }
+  else {
+    // set response data to 'Not found'
+    response = { data: null, error: true, error_message: 'Not found', error_code: 404 };
+  }
 
-	// return response data
-	return response;
+  // return response data
+  return response;
 };
--- react-app/src/components/chains/index.js
+++ react-app/src/components/chains/index.js
@@ -4,62 +4,62 @@ import { Row, Col, Card, CardImg, CardBody, CardTitle, CardText } from 'reactstr
 import Moment from 'react-moment';
 
 const Chains = props => {
-	const [chainsData, setChainsData] = useState([]);
-	const [loaded, setLoaded] = useState(false);
+  const [chainsData, setChainsData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
-	useEffect(() => {
-		const getData = async () => {
-			try {
-				const response = await covalentRequest('/chains/status/');
-				if (response) {
-					setChainsData(response.data ? response.data.items : []);
-				}
-				if (!loaded) {
-					setLoaded(true);
-				}
-			} catch (error) {}
-		};
-		getData();
-		const interval = setInterval(() => getData(), 10 * 1000);
-		return () => clearInterval(interval);
-	}, [loaded]);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await covalentRequest('/chains/status/');
+        if (response) {
+          setChainsData(response.data ? response.data.items : []);
+        }
+        if (!loaded) {
+          setLoaded(true);
+        }
+      } catch (error) {}
+    };
+    getData();
+    const interval = setInterval(() => getData(), 10 * 1000);
+    return () => clearInterval(interval);
+  }, [loaded]);
 
-	return (
-		<div className="mx-4">
-			<h1>{"Example React App"}</h1>
-			{!loaded ?
-				<div className="d-flex align-items-center justify-content-center" style={{ minHeight: '40vh' }}>
-					<div className="spinner-border">
-						<span className="sr-only" />
-					</div>
-				</div>
-				:
-				<Row className="mt-4">
-					{chainsData && chainsData.map((chainData, key) => (
-						<Col key={key} xl="3" lg="4" md="6" xs="12" className="mb-4">
-							<Card className="p-3">
-								<CardImg top src={chainData.logo_url} alt={chainData.name} style={{ maxWidth: '4rem' }} />
-								<CardBody className="p-0 pt-3">
-									<CardTitle tag="h5" className="text-dark mb-0" style={{ fontWeight: 600, textAlign: 'left' }}>
-										{chainData.name}
-									</CardTitle>
-									<CardText className="text-muted mb-2" style={{ fontSize: '.75rem', fontWeight: 400, textAlign: 'left' }}>
-										{"Chain ID: "}{chainData.chain_id}
-									</CardText>
-									<CardText className="text-dark mb-2" style={{ fontSize: '.85rem', fontWeight: 400, textAlign: 'left' }}>
-										{"Synced Block: "}{chainData.synced_block_height}
-									</CardText>
-									<CardText className="text-muted" style={{ fontSize: '.75rem', fontWeight: 300, textAlign: 'left' }}>
-										<Moment format="[Signed at] MMM D, YYYY LTS">{chainData.synced_blocked_signed_at}</Moment>
-									</CardText>
-								</CardBody>
-							</Card>
-						</Col>
-					))}
-				</Row>
-			}
-		</div>
-	);
+  return (
+    <div className="mx-4">
+      <h1>{"Example React App"}</h1>
+      {!loaded ?
+        <div className="d-flex align-items-center justify-content-center" style={{ minHeight: '40vh' }}>
+          <div className="spinner-border">
+            <span className="sr-only" />
+          </div>
+        </div>
+        :
+        <Row className="mt-4">
+          {chainsData && chainsData.map((chainData, key) => (
+            <Col key={key} xl="3" lg="4" md="6" xs="12" className="mb-4">
+              <Card className="p-3">
+                <CardImg top src={chainData.logo_url} alt={chainData.name} style={{ maxWidth: '4rem' }} />
+                <CardBody className="p-0 pt-3">
+                  <CardTitle tag="h5" className="text-dark mb-0" style={{ fontWeight: 600, textAlign: 'left' }}>
+                    {chainData.name}
+                  </CardTitle>
+                  <CardText className="text-muted mb-2" style={{ fontSize: '.75rem', fontWeight: 400, textAlign: 'left' }}>
+                    {"Chain ID: "}{chainData.chain_id}
+                  </CardText>
+                  <CardText className="text-dark mb-2" style={{ fontSize: '.85rem', fontWeight: 400, textAlign: 'left' }}>
+                    {"Synced Block: "}{chainData.synced_block_height}
+                  </CardText>
+                  <CardText className="text-muted" style={{ fontSize: '.75rem', fontWeight: 300, textAlign: 'left' }}>
+                    <Moment format="[Signed at] MMM D, YYYY LTS">{chainData.synced_blocked_signed_at}</Moment>
+                  </CardText>
+                </CardBody>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      }
+    </div>
+  );
 }
 
 export default Chains;
