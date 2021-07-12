9aa8a8a3a03ecb2845c19ff1d0e7434cf015c24c
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-07-12 13:59:38 +0700

    styling

--- react-app/src/components/address/index.js
+++ react-app/src/components/address/index.js
@@ -580,7 +580,7 @@ const Address = props => {
             <h3 className="d-flex align-items-center mb-0" style={{ fontWeight: 600 }}>
               {"Transactions"}
               {!transactionsFullLoaded && transactionsLoading && (
-                <Loader type="Oval" color="#0b5ed7" width="2rem" height="2rem" style={{ marginTop: '-.125rem', marginLeft: '.5rem' }} />
+                <Loader type="Oval" color="#0b5ed7" width="1.5rem" height="1.5rem" style={{ marginTop: '-.125rem', marginLeft: '.5rem' }} />
               )}
             </h3>
           </Col>
